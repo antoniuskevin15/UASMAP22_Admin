52ee1308ec21d181bd841e580615b2d5628b9c96
Vallencius <vallencius1901@gmail.com>
2022-11-20 23:12:46 +0700

    Profile

--- app/src/main/java/id/ac/umn/uasmap22_admind/ProfileActivity.java
+++ app/src/main/java/id/ac/umn/uasmap22_admind/ProfileActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +26,21 @@ public class ProfileActivity extends AppCompatActivity {
 
         ImageView menuBtn = findViewById(R.id.menu_button);
         menuBtn.setOnClickListener(view -> finish());
+
+        if (user != null) {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        } else {
+            // No user is signed in
+        }
     }
 }
--- app/src/main/java/id/ac/umn/uasmap22_admind/ProfileFragment.java
+++ app/src/main/java/id/ac/umn/uasmap22_admind/ProfileFragment.java
@@ -2,11 +2,29 @@ package id.ac.umn.uasmap22_admind;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +32,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
-
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,7 +76,56 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (user != null) {
+            String uid = user.getUid();
+            getUser(uid);
+        } else {
+            // No user is signed in
+        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    public void getUser(String uid){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                db.collection("partner").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("TAG", "DocumentSnapshot data: " + document.getString("name"));
+
+                        TextView tv_nama = (TextView) getView().findViewById(R.id.profile_nama);
+                        tv_nama.setText(document.getString("name"));
+
+                        TextView tv_alamat = (TextView) getView().findViewById(R.id.profile_alamat);
+                        tv_alamat.setText(document.getString("alamat"));
+
+                        TextView tv_type = (TextView) getView().findViewById(R.id.profile_type);
+                        tv_type.setText(": " + document.getString("type"));
+
+                        TextView tv_phone = (TextView) getView().findViewById(R.id.profile_phone);
+                        tv_phone.setText(": " + document.getString("phone"));
+
+                        TextView tv_email = (TextView) getView().findViewById(R.id.profile_email);
+                        tv_email.setText(": " + user.getEmail());
+                    } else {
+                        Log.d("TAG", "No such document");
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                }
+            }
+        });
+
+        db.collection("partner").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+
+            }
+        });
     }
 }
