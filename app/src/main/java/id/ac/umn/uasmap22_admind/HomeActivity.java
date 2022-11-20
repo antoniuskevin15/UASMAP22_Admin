ce9ed54671bc4d3d6511655a0c092850facd3d45
Vallencius <vallencius1901@gmail.com>
2022-11-20 21:30:17 +0700

    Login & Fragment

--- app/src/main/java/id/ac/umn/uasmap22_admind/HomeActivity.java
+++ app/src/main/java/id/ac/umn/uasmap22_admind/HomeActivity.java
@@ -1,14 +1,52 @@
 package id.ac.umn.uasmap22_admind;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import id.ac.umn.uasmap22_admind.databinding.ActivityHomeBinding;
+import id.ac.umn.uasmap22_admind.databinding.ActivityMainBinding;
+
 public class HomeActivity extends AppCompatActivity {
 
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.action_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.action_customize:
+                    replaceFragment(new CustomizeFragment());
+                    break;
+                case R.id.action_notify:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.action_order:
+                    replaceFragment(new OrderFragment());
+                    break;
+                case R.id.action_profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/id/ac/umn/uasmap22_admind/MainActivity.java
+++ app/src/main/java/id/ac/umn/uasmap22_admind/MainActivity.java
@@ -1,29 +1,92 @@
 package id.ac.umn.uasmap22_admind;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        EditText email = (EditText) findViewById(R.id.et_email);
+        EditText password = (EditText) findViewById(R.id.et_password);
+
         TextView loginBtn = (TextView) findViewById(R.id.button);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                out();
-                Intent intentHome = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intentHome);
+//                Toast.makeText(MainActivity.this, email.getText().toString()==nul, Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(email.getText().toString()) || TextUtils.isEmpty(password.getText().toString())){
+                    Toast.makeText(MainActivity.this, "Masukkan email/password!", Toast.LENGTH_SHORT).show();
+                }else{
+                    signIn(email.getText().toString(), password.getText().toString());
+                }
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            out();
+                            Intent intentHome = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intentHome);
+                        } else {
+                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Email/password salah!",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void reload() { }
+
+    private void updateUI(FirebaseUser user) {
     }
 
     public void out(){
